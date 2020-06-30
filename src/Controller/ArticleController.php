e377bd2a2db52889554802041f023799ea0ec7bb
Davide <davide.roberti89@gmail.com>
2020-06-30 19:04:54 +0200

    homepage e rotta all'articolo

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -9,16 +9,16 @@ use Symfony\Component\HttpFoundation\Response;
 class ArticleController extends AbstractController
 {
     /**
-    *@Route("/")
+    *@Route("/", name="app_homepage")
     */
 
     public function homepage()
     {
-        return new Response('bellaaaaaaaa');
+        return $this->render('article/homepage.html.twig');
     }
 
     /**
-    *@Route("/news/{slug}")
+    *@Route("/news/{slug}", name="article_show")
     */
 
     public function show($slug)
