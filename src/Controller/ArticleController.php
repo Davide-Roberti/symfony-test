98d45b4729f7f122eeb746304bf6fe4b4fa87497
Davide <davide.roberti89@gmail.com>
2020-06-30 11:42:20 +0200

    giochini con le rotte

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,13 +2,30 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 
 class ArticleController
 {
+    /**
+    *@Route("/")
+    */
+
     public function homepage()
     {
         return new Response('bellaaaaaaaa');
+    }
+
+    /**
+    *@Route("/news/{slug}")
+    */
+
+    public function show($slug)
+    {
+        return new Response(sprintf(
+            'i am in an existencial crysis, %s',
+            $slug
+        ));
     }
 }
 
