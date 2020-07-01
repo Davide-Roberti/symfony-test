76124c805a554be01e11e5fea0b6608ce4d30cea
Davide <davide.roberti89@gmail.com>
2020-07-01 18:11:59 +0200

    cache

---
+++ public/js/article_show.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $('.js-article-like').on('click', function(){
+
+
+        $(this).toggleClass('fa-heart-o').toggleClass('fa-heart');
+
+        $.ajax({
+            method: 'POST',
+            url: $(this).attr('href')
+        }).done(function(data){
+            $('.js-article-like-count').html(data.hearts);
+        });
+    });
+
+});
--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
+use Psr\Cache\CacheItemPoolInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -21,7 +24,7 @@ class ArticleController extends AbstractController
     *@Route("/news/{slug}", name="article_show")
     */
 
-    public function show($slug)
+    public function show($slug, MarkdownParserInterface $markdown, CacheItemPoolInterface $cache)
     {
         $comments = [
             'whats the meaning of life dude?',
@@ -29,12 +32,40 @@ class ArticleController extends AbstractController
             'maaaan, youre so meeeeeean'
         ];
 
+        $articleContent = <<<EOF
+Lorem ipsum dolor sit amet, **consectetur** adipisicing elit, sed do [eiusmod](https://lego.com) tempor incididunt ut **labore** et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+EOF;
+
+        $item = $cache->getItem('markdown_'.md5($articleContent));
+        if(!$item->isHit()) {
+            $item->set($markdown->transform($articleContent));
+            $cache->save($item);
+        }
+
+        $articleContent = $item->get();
+
 
         return $this->render('article/show.html.twig', [
             'title' => ucwords(str_replace('-', ' ', $slug)),
+            'articleContent' => $articleContent,
+            'slug' => $slug,
             'comments' => $comments,
         ]);
     }
+
+    /**
+    *@Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
+    */
+
+    public function toggleArticleHeart($slug, LoggerInterface $logger)
+    {
+        //DATABASE DA FARE
+        $random = array('hearts' => rand(5, 100));
+        $logger -> info('articolo cuorato');
+
+        return new Response(json_encode($random));
+    }
+
 }
 
- ?>
+?>
